cf46a247840698c023df67e1603808a32cd0c873
Minsung Kim <alstjddl0513@gmail.com>
2026-04-08 16:32:41 +0900

    Fix: 04-08

---
+++ 민성/index.js
@@ -0,0 +1,15 @@
+console.log('1' + 1);
+console.log(1 + '1');
+console.log('1' - 1);
+
+console.log(Boolean('0'));
+console.log(Boolean(0));
+
+console.log(typeof []);
+console.log(Array.isArray([]));
+
+let a;
+let b = null;
+
+console.log(typeof a);
+console.log(typeof b);
