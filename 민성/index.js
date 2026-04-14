cdf63f040af20a84959d21fa69fccb19d1de0544
Minsung Kim <alstjddl0513@gmail.com>
2026-04-14 16:18:06 +0900

    Fix: 04-14

--- 민성/index.js
+++ 민성/index.js
@@ -1,15 +1,37 @@
-console.log('1' + 1);
-console.log(1 + '1');
-console.log('1' - 1);
+// console.log('1' + 1);
+// console.log(1 + '1');
+// console.log('1' - 1);
 
-console.log(Boolean('0'));
-console.log(Boolean(0));
+// console.log(Boolean('0'));
+// console.log(Boolean(0));
 
-console.log(typeof []);
-console.log(Array.isArray([]));
+// console.log(typeof []);
+// console.log(Array.isArray([]));
 
-let a;
-let b = null;
+// let a;
+// let b = null;
 
-console.log(typeof a);
-console.log(typeof b);
+// console.log(typeof a);
+// console.log(typeof b);
+
+let result = 'hello' + false;
+console.log(`${result}, ${typeof result}`);
+
+result = 'hello' + null;
+console.log(`${result}, ${typeof result}`);
+
+result = 6 + undefined;
+console.log(`${result}, ${typeof result}`);
+
+const number1 = 0.1 + 0.2;
+console.log(number1.toFixed(2));
+
+const weather = '맑음';
+const temp = 21;
+const message = `오늘의 날씨는 ${weather}\n현재 기온은 ${temp}도입니다.`;
+
+console.log(message);
+
+console.log(String.raw`오늘의 날씨는 ${weather}\n현재 기온은 ${temp}도입니다.`);
+
+console.log(String.raw`${message}`);
